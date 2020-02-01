89ad981549840384b7ad6d95c07ee139827f1163
DeveloperNate <43491983+DeveloperNate@users.noreply.github.com>
2020-02-01 10:36:34 +0000

    Update hearts.php

--- Video 2 - Adding CSS  and Javascript/hearts.php
+++ Video 2 - Adding CSS  and Javascript/hearts.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * @package Nathan 
+ * @package Nathan
  */
 /*
-Plugin Name: Hearts Title 
-Plugin URI: https://akismet.com/
-Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from spam</strong>. It keeps your site protected even while you sleep. To get started: activate the Akismet plugin and then go to your Akismet Settings page to set up your API key.
-Version: 4.1.3
-Author: Automattic
-Author URI: https://automattic.com/wordpress-plugins/
-License: GPLv2 or later
-Text Domain: akismet
+Plugin Name: Hearts Title
+Plugin URI: none
+Description:  This is plugin about hearts
+Version: 0.1
+Author: Nathan
+Author URI: 
+License: GPLv2 
+Text Domain: Nathan 
 */
 
 /*
@@ -28,11 +28,19 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-Copyright 2005-2015 Automattic, Inc.
+
 */
 
 // Make sure we don't expose any info if called directly
+
 if ( !function_exists( 'add_action' ) ) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
+}
+
+class Heart{
+
+    function __construct(){
+        // any code that is in here will be executed 
+    }
 }
