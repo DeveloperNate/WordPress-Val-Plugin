2c3d5fea45e0c4187fb90ab681b1236ab5cb4284
DeveloperNate <43491983+DeveloperNate@users.noreply.github.com>
2020-02-13 13:54:15 +0000

    Update HeartWidget.php

--- Video 8 - Adding jQuery to WordPress/includes/HeartWidget.php
+++ Video 8 - Adding jQuery to WordPress/includes/HeartWidget.php
@@ -23,9 +23,18 @@ class Val_Widget extends WP_Widget {
      * @param array $instance Saved values from database.
      */
     public function widget( $args, $instance ) {
+
+        $widgetOptions = get_option('optionName'); // gets data from the setting page 
+
+        $title = ! empty( $widgetOptions['title_string'] ) ? $widgetOptions['title_string'] : 'Heart';
+		$animation = ! empty( $widgetOptions['animation_string'] ) ? $widgetOptions['animation_string'] : 'fading';
+		$heart = ! empty( $widgetOptions['heart_string'] ) ? $widgetOptions['heart_string'] : 'normal'; // if statments to add default values if no value is found
         
         echo $before_widget;
-       
+
+       echo '<div class="textcontainer">
+				<span class="hearts '.$animation.'" data-number-hearts='.$heart.' >'.$title.'</span>
+		  	</div>';// add my containers and span tag
         
         echo $after_widget;
     }
